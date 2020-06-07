f19a603b4ccefb6bf4e1447d5a2520408533ce77
eselenic <selenicaelona@gmail.com>
2020-06-07 17:51:42 -0500

    Updates

---
+++ precache-manifest.f264f22bfe207ad2063234e2578f3623.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b96be879ed520926ec3e2ebe4376df3",
+    "url": "/Web-Apps-Final/index.html"
+  },
+  {
+    "revision": "25f326e20d1a66b31368",
+    "url": "/Web-Apps-Final/static/css/main.f2c41f9e.chunk.css"
+  },
+  {
+    "revision": "ee4c55ba284bb1ec5ae5",
+    "url": "/Web-Apps-Final/static/js/2.398b2bbe.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/Web-Apps-Final/static/js/2.398b2bbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25f326e20d1a66b31368",
+    "url": "/Web-Apps-Final/static/js/main.a5305033.chunk.js"
+  },
+  {
+    "revision": "9f7d8f149b3da847d7a5",
+    "url": "/Web-Apps-Final/static/js/runtime-main.67873a78.js"
+  }
+]);
